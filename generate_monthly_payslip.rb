ed933334e168dacc4ee3861774c840535863f07e
Jake <yeemunhong@gmail.com>
2022-02-18 18:42:08 +0800

    added .bat file to call payslip program with args

--- generate_monthly_payslip.rb
+++ generate_monthly_payslip.rb
@@ -1,15 +1,18 @@
-require_relative "calculate_income_tax"
-
-salary = 80_150
-tax_bracket = [
+require_relative "income_tax"
+# Predefine the Tax Bracket
+# If args is passed, use th passed args
+tax_bracket = ARGV[2].nil? ? [
   [40, 180_000],
   [30, 80_000],
   [20, 40_000],
   [10, 20_000],
   [0, 0],
-]
-total_tax = calculate_income_tax(salary, tax_bracket)
+] : ARGV[2]
 
-puts "Total Tax: #{calculate_income_tax(60000, tax_bracket)}"
-puts "Total Tax: #{calculate_income_tax(200000, tax_bracket)}"
-puts "Total Tax: #{calculate_income_tax(80150, tax_bracket)}"
+ren_income_tax = IncomeTax.new(ARGV[0], ARGV[1].to_i, tax_bracket, !ARGV[2].nil?)
+puts "----------------------------------------------------------"
+puts "Monthly Payslip for: \"#{ren_income_tax.name}\""
+puts sprintf("Gross Monthly Income: $%.2f", ren_income_tax.annual_gross_salary / 12)
+puts sprintf("Monthly Income Tax: $%.2f", ren_income_tax.annual_income_tax / 12)
+puts sprintf("Net Monthly Income: $%.2f", ren_income_tax.net_monthly_income)
+puts "----------------------------------------------------------"
